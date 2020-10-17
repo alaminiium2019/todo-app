f01721aae024169ab77e2dced14a9eaf08f4e9e9
alaminiium2019 <alamin.iium2017@gmail.com>
2020-10-17 15:38:49 +0600

    first commint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]); //for set value
+  const [input, setInput] = useState([]);//for input value
+
+  const addTodo = (event) => {
+    event.preventDefault();   //will stop the refresh
+    console.log("I am working")
+    setTodos([...todos, input]); //...name is previous data, input new data
+    setInput('');  //clear up the input after clicking
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form>
+        <h1>Hello Prgrammers world: {todos.length}</h1>
+        <input value={input} onChange={event => setInput(event.target.value)}></input>
+        <button  type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
+      <ul>
+        {
+          todos.map(todo => <li>{todo}</li>)
+        }
+      </ul>
+
+    </div >
   );
 }
 
